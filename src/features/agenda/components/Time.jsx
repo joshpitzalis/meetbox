aeadef6497b5236b4aaa1b76f9c6b73a6964ed6f
joshpitzalis <joshpitzalis@gmail.com>
2019-07-24 14:24:55 +0530

    timer module beginnings

--- src/features/agenda/components/Time.jsx
+++ src/features/agenda/components/Time.jsx
@@ -1,23 +1,48 @@
 import React, { useState } from "react";
+import { fromEvent } from "rxjs";
 
 const ItemTime = ({ minutes }) => {
   const [time, setTime] = useState(minutes);
   const [error, setError] = useState("");
+
+  const inc = React.useRef(null);
+  const dec = React.useRef(null);
+
+  React.useEffect(() => {
+    const inc$ = fromEvent(inc.current, "click").subscribe(event => {
+      setError("");
+      setTime(time + 5);
+    });
+    const dec$ = fromEvent(dec.current, "click").subscribe(event => {
+      setError("");
+      // if (time === 5) {
+      //   setError("5 is the minimum.");
+      //   setTimeout(() => setError(""), 1000);
+      //   return;
+      // }
+      setTime(time - 5);
+    });
+    return () => {
+      inc$.unsubscribe();
+      dec$.unsubscribe();
+    };
+  }, [time]);
   return (
     <div className="flex flex-col justify-center">
       <div className="flex items-center mr4-l">
         <div
           className="w1 pointer"
           data-testid="dec"
-          onClick={() => {
-            setError("");
-            if (time === 5) {
-              setError("5 is the minimum.");
-              setTimeout(() => setError(""), 1000);
-              return;
-            }
-            setTime(time - 5);
-          }}
+          // onClick={() => {
+          //   setError("");
+          //   if (time === 5) {
+          //     setError("5 is the minimum.");
+          //     setTimeout(() => setError(""), 1000);
+          //     return;
+          //   }
+          //   setTime(time - 5);
+          // }}
+          ref={dec}
         >
           -
         </div>
@@ -28,10 +53,11 @@ const ItemTime = ({ minutes }) => {
         <div
           className="w1 pointer"
           data-testid="inc"
-          onClick={() => {
-            setError("");
-            setTime(time + 5);
-          }}
+          // onClick={() => {
+          //   setError("");
+          //   setTime(time + 5);
+          // }}
+          ref={inc}
         >
           +
         </div>
