d5d76e3d5a59b563caf4bd9684ed81443e1fcc8b
joshpitzalis <joshpitzalis@gmail.com>
2019-08-07 16:53:23 +0530

    authentication module updated

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,19 +1,14 @@
 import { useMachine } from "@xstate/react";
 import { Button } from "grommet";
-import { Google } from "grommet-icons";
+import { Google, Notes, Schedule, Task } from "grommet-icons";
 import React from "react";
 import ReactGA from "react-ga";
 import { authState } from "rxfire/auth";
 import { notfication$ } from "../components/Banner";
-import rocket from "../styles/images/rocket.svg";
 import firebase from "../utilities/firebase";
 import stateMachine from "../utilities/statechart";
 
-authState(firebase.auth()).subscribe(user => {
-  console.log(user, " will be null if logged out");
-});
-
-const Landing = ({ match, history }) => {
+const Landing = ({ history }) => {
   const [, send] = useMachine(stateMachine);
 
   React.useEffect(() => {
@@ -46,50 +41,18 @@ const Landing = ({ match, history }) => {
             Turn team meetings into action plans.
           </h1>
 
-          <Button
-            icon={<Google color="plain" />}
-            label="Get Started"
-            onClick={async () => {
-              const { gapi } = window;
-              const { auth } = firebase;
-              try {
-                const googleAuth = gapi.auth2.getAuthInstance();
-
-                const googleUser = await googleAuth.signIn();
-
-                const token = googleUser.getAuthResponse().id_token;
-
-                const credential = auth.GoogleAuthProvider.credential(token);
-
-                await auth().signInWithCredential(credential);
-
-                ReactGA.event({
-                  category: "User",
-                  action: "NEW_AGENDA_CREATED"
-                });
-
-                send({ type: "NEW_AGENDA_CREATED", payload: history });
-              } catch (error) {
-                console.log("error", error, typeof error);
-                const message = error.message || error;
-                notfication$.next({
-                  type: "ERROR",
-                  message
-                });
-              }
-            }}
-          />
+          <Start firebase={firebase} send={send} history={history} />
         </header>
-
-        <article className="mw5 mw6-ns center pt4 list pl0 w-50-ns w-100 dn db-l flex items-end justify-end dn db-l">
+        <Features />
+        {/* <article className="mw5 mw6-ns center pt4 list pl0 w-50-ns w-100 dn db-l flex items-end justify-end dn db-l">
+          
+          
           <img
             className="w-auto h-50 mb3 pb4"
             src={rocket}
             alt="team rocket building"
           />
-        </article>
-
-        {/* <Features /> */}
+        </article> */}
       </article>
     </div>
   );
@@ -97,37 +60,115 @@ const Landing = ({ match, history }) => {
 
 export default Landing;
 
-function Features({}) {
+function Features() {
   return (
-    <ul className="list pl0 w-50-ns w-100 dn db-l">
-      <li className="pa3 pa4-ns mv4 item0">
+    <ul className="list pl0 w-50-ns w-100 dn db-l mt3">
+      <li className="pa3 pa4-ns mv4 item0 flex x">
+        <Schedule className="ma3" size="large" />
         <b className="db f3 mb1 text-gray-700 h3">
-          📅 Use it to create and collaborate on an agenda, then send out google
+          Use it to create and collaborate on an agenda, then send out google
           calendar invites.
         </b>
-        {/* <span className="f5 db lh-copy measure">
-       Use Meetbox to draft a list of everything you want to discuss in
-       your next meeting. You can share teh draft with other people that
-       might want to add to teh agenda.
-      </span> */}
       </li>
-      <li className="pa3 pa4-ns mv4 item1 br2">
-        <b className="db f3 mb1 text-gray-700 h3">
-          📝 Then take notes on what happens during the meeting.
+      <li className="pa3 pa4-ns mv4 item1 br2 flex x">
+        <Notes className="ma3" size="large" />
+        <b className="db f3 mt2 text-gray-700 h3">
+          Then take notes on what happens during the meeting.
         </b>
       </li>
-      <li className="pa3 pa4-ns mv4 item2 br2 ">
+      <li className="pa3 pa4-ns mv4 item2 br2 flex x ">
+        <Task className="ma3 " size="large" />
         <b className="db f3 mb1 text-gray-700 h3">
-          ✅ Instantly share everything when the meeting is over so that
-          everyone knows what they have to do.
+          Instantly share everything when the meeting is over so that everyone
+          knows what they have to do.
         </b>
-        {/* <span className="f5 db lh-copy measure">
-       When you start a meeting anyone can take notes and assign tasks on
-       each agenda item. You can also park ideas at the bottom of the
-       agenda during teh meeting. That way you can stay on point without
-       constantly addressing tangents.
-      </span> */}
       </li>
     </ul>
   );
+}
+
+function Start({ firebase, send, history }) {
+  const [user, setUser] = React.useState("loading");
+
+  React.useEffect(() => {
+    const userListener = authState(firebase.auth()).subscribe(user =>
+      setUser(user)
+    );
+    return () => userListener.unsubscribe();
+  }, []);
+
+  if (user === "loading") {
+    return <Button label="Loading..." plain disbaled />;
+  } else if (user) {
+    return (
+      <>
+        <Button
+          primary
+          label="Create An Agenda To Begin"
+          onClick={() => {
+            ReactGA.event({
+              category: "User",
+              action: "NEW_AGENDA_CREATED"
+            });
+            send({
+              type: "NEW_AGENDA_CREATED",
+              payload: history
+            });
+          }}
+        />
+        <Button
+          plain
+          label={<small className="dark-red small-caps ml3">Logout</small>}
+          onClick={async () => {
+            const { auth } = firebase;
+            try {
+              await auth().signOut();
+            } catch (error) {
+              const message = error.message || error;
+              notfication$.next({
+                type: "ERROR",
+                message
+              });
+            }
+          }}
+        />
+      </>
+    );
+  } else {
+    return (
+      <Button
+        icon={<Google color="plain" className="ma1" />}
+        label="Get Started"
+        onClick={async () => {
+          const { gapi, analytics } = window;
+          const { auth } = firebase;
+          try {
+            const googleAuth = gapi.auth2.getAuthInstance();
+            const googleUser = await googleAuth.signIn();
+            const token = googleUser.getAuthResponse().id_token;
+            const credential = auth.GoogleAuthProvider.credential(token);
+            const signedIn = await auth().signInWithCredential(credential);
+            console.log("signedIn", signedIn);
+            const newUser = false;
+            if (signedIn) {
+              const { user } = signedIn;
+
+              if (newUser) {
+                analytics.track("new_user", {
+                  name: user.displayName,
+                  email: user.email
+                });
+              }
+            }
+          } catch (error) {
+            const message = error.message || error;
+            notfication$.next({
+              type: "ERROR",
+              message
+            });
+          }
+        }}
+      />
+    );
+  }
 }
